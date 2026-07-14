cd2efecf941f8f1bfdd5bb539d12b12bcd356941
unknown <raupersan>
2026-07-14 12:52:16 +0200

    clase telefono

--- telefono.py
+++ telefono.py
@@ -1,2 +1,17 @@
 class Telefono:
-    
+    def __init__(self):
+        self.__telefonoFijo = 0
+        self.__telefonoMovil = 0
+        self.__telefonoTrabajo = 0
+    def getTelefonoFijo(self):
+        return self.__telefonoFijo
+    def getTelefonoMovil(self):
+        return self.__telefonoMovil
+    def getTelefonoTrabajo(self):
+        return self.__telefonoTrabajo
+    def setTelefonoFijo(self, numero):
+        self.__telefonoFijo = numero
+    def setTelefonoMovil(self, numero):
+        self.__telefonoMovil = numero
+    def setTelefonoTrabajo(self, numero):
+        self.__telefonoTrabajo = numero
