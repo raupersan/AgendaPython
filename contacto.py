7d3322ab66214bcbfc82c660d247cb345b30f41a
unknown <raupersan>
2026-07-14 13:04:51 +0200

    clase contacto

---
+++ contacto.py
@@ -0,0 +1,25 @@
+from direccion import Direccion
+from persona import Persona
+from telefono import Telefono
+
+class contacto(Direccion, Persona, Telefono):
+    def __init__(self):
+        super().__init__()
+        self.__email = ""
+    def getEmail(self):
+        return self.__email
+    def setEmail(self, email):
+        self.__email = email
+    def mostrarContacto(self):
+        print("\tNomrbre del contacto: ", self.getNombre)
+        print("\tApellidos del contacto: ", self.getApellidos)
+        print("\tFecha de nacimiento del contacto: ", self.getFechaNacimiento)
+        print("\tCorreo del contacoto: ", self.getEmail)
+        print("\tCalle del contacto: ", self.getCalle)
+        print("\tPiso del contacto: ", self.getPiso)
+        print("\tCiudad del contacto: ", self.getCiudad)
+        print("\tCódigo postal del contacto: ", self.getCodigoPostal)
+        print("\tTeléfono fijo del contacto: ", self.getTelefonoFijo)
+        print("\tTelefono móvil del contacto: ", self.getTelefonoMovil)
+        print("\tTelefono de trabajo del contacto: ", self.getTelefonoTrabajo)
+
